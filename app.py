71da176b3b8ca72f9eddb8be1c93bdd0f9f0891f
carol_rosa <caroline.rosa.silva@gmail.com>
2021-04-20 20:20:18 -0300

    config cors

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 from resources.routes import controller
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(controller)
 app.config['JSON_AS_ASCII'] = False
 
