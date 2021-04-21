1139a845848268758999533cc794e94a557b1a7b
carol_rosa <caroline.rosa.silva@gmail.com>
2021-04-21 14:52:16 -0300

    improve get disciplinas object response

--- resources/service.py
+++ resources/service.py
@@ -6,20 +6,34 @@ def get_all_cursos():
     for c in cursos:
         obj = {'Id': c['IdCurso'], 'NomeCurso':c['NomeCurso']}
         listResult.append(obj)
+
     return listResult
 
 def get_all_disciplinas_by_curso(id):
     disciplinas = get_disciplinas_by_curso(id)
     listResult = []
     for d in disciplinas:
-        obj = { 
-            'IdCurso': d['IdCurso'],
-            'NomeDisciplina': d['NomeDisciplina'],
-            'CodigoDisciplina': d['CodigoDisciplina'],
-            'NumAulas': d['NumAulas'],
-            'Turmas': d['Turmas']
+        listTurmas = []
+        for t in d['Turmas']:
+            obj_turma = {
+                t['CodigoTurma']: {
+                    'prof': t['Professor'],
+                    'horarios_str': ", ".join(t['Horario']),
+                    'horarios': [s.split('-')[0] for s in t['Horario']],
+                    'salas': [s.split('-')[1] for s in t['Horario']]
+                }
+            }
+            listTurmas.append(obj_turma)
+        obj_disc = { 
+            d['CodigoDisciplina']: {
+                'IdCurso': d['IdCurso'],
+                'nome': d['NomeDisciplina'],
+                'aulas': d['NumAulas'],
+                'turmas': listTurmas
+            }
         }
-        listResult.append(obj)
+        listResult.append(obj_disc)
+
     return listResult
 
 def get_grades(data):
