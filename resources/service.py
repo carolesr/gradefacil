6170f93b078defabcd05efccecd6cc513a0f6f09
carol_rosa <caroline.rosa.silva@gmail.com>
2021-04-22 18:16:16 -0300

    use unordered dict

--- resources/service.py
+++ resources/service.py
@@ -33,12 +33,13 @@ def get_all_disciplinas_by_curso(id):
 
     dict_ordered =  sorted(dict_final.items(), key=lambda x: x[1]['nome'])
 
-    return dict_ordered
+    return dict_final
 
 
 def montar_grades(obj):
     print(obj)
-    size = len(obj)
+    size = len(obj.items())
+    print(size)
     for i in range(size):
         print(obj[i])
 
@@ -46,19 +47,28 @@ def get_grades(data):
     idcurso = data['idCurso']
     dictTurmas = data['dicts']
     listDisc = list(dictTurmas.keys())
-    print(listDisc)
     disciplinas = get_turmas_by_curso_and_disc(idcurso, listDisc)
 
-    listFinal = []
+    dict_final = {}
     for d in disciplinas:
-        obj = { 
-            'IdCurso': d['IdCurso'],
-            'NomeDisciplina': d['NomeDisciplina'],
-            'CodigoDisciplina': d['CodigoDisciplina'],
-            'NumAulas': d['NumAulas'],
-            'Turmas': [t for t in d['Turmas'] if t['CodigoTurma'] in dictTurmas[d['CodigoDisciplina']]]
-        }
-        listFinal.append(obj)
 
-    montar_grades(listFinal)
-    return listFinal
+        dict_turmas = {}
+        turmas_filtradas = [t for t in d['Turmas'] if t['CodigoTurma'] in dictTurmas[d['CodigoDisciplina']]]
+        for t in turmas_filtradas:
+            dict_turmas[t['CodigoTurma']] = {
+                    'prof': t['Professor'],
+                    'horarios_str': ", ".join(t['Horario']),
+                    #'horarios': [s.split('-')[0] for s in t['Horario']],
+                    #'salas': [s.split('-')[1] for s in t['Horario']]
+                }
+
+        dict_final[d['CodigoDisciplina']] = {
+                #'idcurso': d['IdCurso'],
+                'nome': d['NomeDisciplina'],
+                'aulas': d['NumAulas'],
+                'turmas': dict_turmas
+            }
+
+    montar_grades(dict_final)
+
+    return dict_final
